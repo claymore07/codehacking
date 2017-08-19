a9b6b4883b5425ccf5ad09ff488d915dc9d59a1f
claymore07 <babak_nouri67@yahoo.com>
2017-08-19 21:57:51 +0430

    migration and relations added in this phase role table foreign key form roles to users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,13 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->integer('role_id')->unsigned();
+            $table->integer('is_active')->default(0);
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
         });
     }
 
